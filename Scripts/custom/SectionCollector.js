949e7f00cc198da456815d10192002477aba9cac
Harrozze Lee <harrozze@gmail.com>
2023-06-20 15:42:21 +0800

    fixed: ignore lines with no content (only leading number)

--- Scripts/custom/SectionCollector.js
+++ Scripts/custom/SectionCollector.js
@@ -1,7 +1,9 @@
 class SectionCollector {
     async show(app, dv, section_name, tag_name) {
         let page_filter_pattern = new RegExp(`## ${section_name}`);
-        let line_filter_pattern = new RegExp(`^## ${section_name}`);
+        let header_filter_pattern = new RegExp(`^## ${section_name}`);
+        let no_content_line_filter_pattern = new RegExp('^[ \t]*[0-9]+\.[ \t]\+$');
+        let empty_line_filter_pattern = new RegExp('^[ \t]*$');
         let image_wiki_link_filters_patterns = [
             new RegExp(/!\[[^\]]*\]\(([^\]]*)\)/g),
             new RegExp(/!\[\[([^\]]*)\]\]/g),
@@ -13,7 +15,10 @@ class SectionCollector {
             if (p.file.folder.match("^Templates")) {
                 continue;
             }
-            await dv.io.load(p.file.path).then(content => {
+            let page_file_path = p.file.path;
+            let non_empty_lines = 0;
+            let lines_in_page = [];
+            await dv.io.load(page_file_path).then(content => {
                 return content.match(page_filter_pattern) ?  content : "";
             }).then(content => {
                 let lines = content.split("\n");
@@ -23,40 +28,57 @@ class SectionCollector {
                         found = false;
                         break;
                     }
-                    if (line.match(line_filter_pattern)) {
+                    if (line.match(header_filter_pattern)) {
                         found = true;
-                        target_pages[p.file.path] = ["## [[" + p.file.name + "]]"];
+                        lines_in_page = ["## [[" + p.file.name + "]]"];
                         continue;
                     }
-                    if (found) {
-                        let has_image = false;
-                        image_wiki_link_filters_patterns.map(pattern => {
-                            let m = [ ...line.matchAll(pattern) ];
-                            m.map(item => {
-                                let image_url = item[1];
-                                if (!image_url.match(/^http/i)) {
-                                    image_url = `file://${app.vault.adapter.basePath}/${image_url}`
-                                }
-                                // console.log(`pattern=${pattern}, image_url=${image_url}`);
-                                target_pages[p.file.path].push(`<img src='${image_url}'>`);
-                                has_image = true;
-                            })
-                        });
-                        if (!has_image) {
-                            target_pages[p.file.path].push(line);
-                        }
+                    if (!found) {
+                        continue;
+                    }
+                    if (line.match(no_content_line_filter_pattern)) {
+                        continue;
+                    }
+                    if (!line.match(empty_line_filter_pattern)) {
+                        ++ non_empty_lines;
+                        console.log(`non-empty-line: ${line}`);
+                    }
+
+                    let has_image = false;
+                    image_wiki_link_filters_patterns.map(pattern => {
+                        let m = [ ...line.matchAll(pattern) ];
+                        m.map(item => {
+                            let image_url = item[1];
+                            if (!image_url.match(/^http/i)) {
+                                image_url = `file://${app.vault.adapter.basePath}/${image_url}`
+                            }
+                            // console.log(`pattern=${pattern}, image_url=${image_url}`);
+                            lines_in_page.push(`<img src='${image_url}'>`);
+                            has_image = true;
+                        })
+                    });
+                    if (!has_image) {
+                        lines_in_page.push(line);
                     }
                 }
-            })
+            });
+            console.log(`found: page=${page_file_path}, non_empty_lines=${non_empty_lines}`);
+            if (non_empty_lines > 0 && lines_in_page.length > 1) {
+                console.log(`found: page=${page_file_path}, lines: ${lines_in_page.length}`);
+                target_pages[page_file_path] = lines_in_page;
+            }
         }
 
+        let sorted_target_page_paths = Object.keys(target_pages).sort().reverse();
+        console.log(`sorted_target_page_paths=${sorted_target_page_paths}`);
+
         let toc = ["## Table of Contents"];
-        for (let p  of Object.keys(target_pages).sort().reverse()) {
+        for (let p of sorted_target_page_paths) {
             toc.push("- [[" + p + "]]");
         }
 
         let content = [toc.join("\n")];
-        for (let p of Object.keys(target_pages).sort().reverse()) {
+        for (let p of sorted_target_page_paths) {
             content.push(target_pages[p].join('\n'));
         }
         dv.el("article", content.join("\n"));
